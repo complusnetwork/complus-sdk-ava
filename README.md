4be04f1323f17f0911b7a445bcac4cad165d706d
cryptosudo <cryptosudo@gmail.com>
2021-01-02 11:56:36 +0300

    Complus | SDK for Avalanche

---
+++ README.md
@@ -0,0 +1,23 @@
+# Complus | SDK FOR AVA
+
+[![code style: prettier](https://img.shields.io/badge/code_style-prettier-ff69b4.svg?style=flat-square)](https://github.com/prettier/prettier)
+[![Actions Status](https://github.com/complusnetwork/complus-sdk-ava/workflows/CI/badge.svg)](https://github.com/complusnetwork/complus-sdk-ava)
+[![npm version](https://img.shields.io/npm/v/@complus/sdk-ava/latest.svg)](https://www.npmjs.com/package/@complus/sdk-ava/v/latest)
+[![npm bundle size (scoped version)](https://img.shields.io/bundlephobia/minzip/@complus/sdk-ava/latest.svg)](https://bundlephobia.com/result?p=@complus/sdk-ava@latest)
+
+## Avalanche C-Chain
+
+- [AVA Docs](https://docs.avax.network/)
+- [AVA Explorer](https://cchain.explorer.avax.network/)
+
+## Local Development
+
+The following assumes the use of `node@>=10`
+
+#### Install Dependencies
+
+`yarn`
+
+#### Run Build
+
+`yarn build`
---
+++ src/constants.ts
@@ -0,0 +1,47 @@
+import JSBI from 'jsbi'
+
+// exports for external consumption
+export type BigintIsh = JSBI | bigint | string
+
+export enum ChainId {
+  MAINNET = 43114,
+  TESTNET = 43113
+}
+
+export enum TradeType {
+  EXACT_INPUT,
+  EXACT_OUTPUT
+}
+
+export enum Rounding {
+  ROUND_DOWN,
+  ROUND_HALF_UP,
+  ROUND_UP
+}
+
+export const FACTORY_ADDRESS = '0x5C02e78A3969D0E64aa2CFA765ACc1d671914aC0'
+
+export const INIT_CODE_HASH = '0x0f75b63316180834cae9ff38fbeb719026e1435e60ddb4509bd435f148feac97'
+
+export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000)
+
+// exports for internal consumption
+export const ZERO = JSBI.BigInt(0)
+export const ONE = JSBI.BigInt(1)
+export const TWO = JSBI.BigInt(2)
+export const THREE = JSBI.BigInt(3)
+export const FIVE = JSBI.BigInt(5)
+export const TEN = JSBI.BigInt(10)
+export const _100 = JSBI.BigInt(100)
+export const _997 = JSBI.BigInt(997)
+export const _1000 = JSBI.BigInt(1000)
+
+export enum SolidityType {
+  uint8 = 'uint8',
+  uint256 = 'uint256'
+}
+
+export const SOLIDITY_TYPE_MAXIMA = {
+  [SolidityType.uint8]: JSBI.BigInt('0xff'),
+  [SolidityType.uint256]: JSBI.BigInt('0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff')
+}
